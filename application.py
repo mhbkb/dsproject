083377d03796b9bc5445ec0be47e84b17c2d059a
mhbkb <mao170549392>
2016-04-22 22:44:15 -0500

    Add templete code.

--- application.py
+++ application.py
@@ -5,5 +5,30 @@ application = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+# How: localhost   curl --data '' http://127.0.0.1:5000/hostname/String/a%20b%20c
+#      online      curl --data '' http://flask3.qst6ftqmmz.us-west-2.elasticbeanstalk.com/hostname/String/a%20b%20c
+# TODO: check param, Call db
+@application.route("/hostname/String/<param>", methods=['POST'])
+def write(param):
+	strs = param.split(' ')
+	# Call db api
+	print str(strs)
+
+	return ', '.join(str(x) for x in strs)
+
+
+# How: localhost   http://127.0.0.1:5000/hostname/Counts/a,b
+#      online      http://flask3.qst6ftqmmz.us-west-2.elasticbeanstalk.com/hostname/Counts/a,b
+# TODO: check param, Call db
+@application.route("/hostname/Counts/<words>", methods=['GET'])
+def read(words):
+	wordsArray = words.split(',')
+	# Call db api
+
+	return ', '.join(str(x) for x in wordsArray)
+
+
+
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
